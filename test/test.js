d44002d17ab2882fbaff36218a84d755c109b90b
Tarikly <tarikly+github@gmail.com>
2022-03-29 21:37:38 -0300

    test: pipeline

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-   it("responds with Hello World!", (done) => {
+   it("responds with Hello World!!", (done) => {
       request(app).get("/").expect("Hello World from Dockerized app!", done);
    })
 });
